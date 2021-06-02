6b0f8c624c69af4df3015c4a212caedc0b13fd18
Mind-Surfer <49238785+Mind-Surfer@users.noreply.github.com>
2021-06-02 17:03:08 +0100

    Fixes. Now runs but needs work

--- docker/run/entrypoint.sh
+++ docker/run/entrypoint.sh
@@ -1,40 +1,18 @@
 #!/bin/sh
-for arg in "$@"
-do
-index=$(echo $arg | cut -f1 -d=)
-val=$(echo $arg | cut -f2 -d=)
-case $index in
-ssh) ssh_file=$val;;
+echo "Using ssh file $SSH_KEY"
+echo "Using inventory file: $INVENTORY"
+echo "Using playbook file: $PLAYBOOK"
 
-hosts) hosts=$val;;
-
-playbook) playbook=$val;;
-
-*)
-esac
-done
-
-if [[ -f $hosts && -f $playbook && ! -z "$ssh_file" && ! -z "$hosts" && ! -z "$playbook" ]]; then
- echo "Using ssh file $ssh_file"
- echo "Using hosts file: $hosts"
- echo "Using playbook file: $playbook"
+if [ -d ~/.ssh ]; then
  #Get rid of whatever was there previously
- rm -r ~/.ssh
- mkdir -p ~/.ssh
- #Copy the new onw in
- cp $ssh_file ~/.ssh
- #Reset the permissions (got this command from https://superuser.com/questions/215504/permissions-on-private-key-in-ssh-folder)
- find .ssh/ -type f -exec chmod 600 {} \;; find .ssh/ -type d -exec chmod 700 {} \;; find .ssh/ -type f -name "*.pub" -exec chmod 644 {} \;
- #Run the playbook
- ansible-playbook $playbook -i $hosts
-
-elif [[ ! -d $ssh_file || -z "$ssh_file" ]]; then
- echo "Could not find ssh file! Exiting.." && exit 1
-
-elif [[ ! -f $hosts || -z "$hosts" ]]; then
- echo "Could not find hosts file! Exiting.." && exit 1
-
-elif [[ ! -f $playbook || -z "$playbook" ]]; then
- echo "Could not find playbook! Exiting.." && exit 1
-
+ rm -r ~/.ssh 
 fi
+mkdir -p ~/.ssh
+#Copy the new one in
+cp -r $SSH_KEY/* ~/.ssh
+ls -a ~/.ssh
+
+#Reset the permissions (got this command from https://superuser.com/questions/215504/permissions-on-private-key-in-ssh-folder)
+#find .ssh/ -type f -exec chmod 600 {} \;; find .ssh/ -type d -exec chmod 700 {} \;; find .ssh/ -type f -name "*.pub" -exec chmod 644 {} \;
+#Run the playbook
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook $PLAYBOOK -i $INVENTORY
